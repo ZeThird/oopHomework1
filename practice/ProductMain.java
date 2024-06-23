76dbac7fab10fc0d38efe2159ce818be119039ee
ZeThird <levbatura@rambler.ru>
2024-06-23 15:35:34 +0300

    Добавил примеров

--- practice/ProductMain.java
+++ practice/ProductMain.java
@@ -4,26 +4,36 @@ import practice.impl.HotDrink;
 import practice.impl.HotDrinkVM;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ProductMain {
 
     public static void main(String[] args) {
         HotDrink hotDrink1 = new HotDrink("milk drink", 10f, LocalDate.of(2005, 9, 3), 55.366f);
-        HotDrink hotDrink2 = new HotDrink("choco drink", 10f, LocalDate.of(2025, 9, 5), 88.531f);
-        
-        HotDrinkVM vendingMachine = new HotDrinkVM();
-        List<Product> products = new ArrayList<Product>();
-        products.add(hotDrink1);
-        products.add(hotDrink2);
+        HotDrink hotDrink2 = new HotDrink("choco drink", 10f, LocalDate.of(2025, 9, 5), 88.531f); 
+        HotDrink hotDrink3 = new HotDrink("coconut drink", 20f, LocalDate.of(2023, 5, 9), 62.371f);
+        HotDrink hotDrink4 = new HotDrink("watermelon drink", 5f, LocalDate.of(2015, 4, 8), 99.103f);
 
-        vendingMachine.addProducts(products);
+        HotDrinkVM vendingMachine1 = new HotDrinkVM();
+        vendingMachine1.addProduct(hotDrink1);
+        vendingMachine1.addProduct(hotDrink2);
+
+
+        HotDrinkVM vendingMachine2 = new HotDrinkVM();
+        vendingMachine2.addProduct(hotDrink3);
+        vendingMachine2.addProduct(hotDrink4);
 
         System.out.printf("hotDrink2: %s\n", hotDrink2.toString());
-        HotDrink foundProduct = vendingMachine.getProduct("milk drink", 1f, 55.366f);
-        if (foundProduct != null) {
-            System.out.printf("Found product: %s\n", foundProduct.toString());
+
+        HotDrink foundProduct = vendingMachine1.getProduct("milk drink", 1f, 55.366f);
+        if (foundProduct != null) {            
+            System.out.printf("Found product in first vending machine: %s\n", foundProduct.toString());
         }
+
+     
+        HotDrink foundProduct2 = vendingMachine2.getProduct("watermelon drink", 1f, 99.103f);
+        if (foundProduct2 != null) {
+            System.out.printf("Found product in second vending machine: %s\n", foundProduct2.toString());
+        }
+    
     }
 }
